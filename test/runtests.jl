4444287d8b7e96a3130cc1a453e7f0ac5c1859fe
d4v1d-cub <david.machado@fisica.uh.cu>
2024-04-28 19:35:52 -0400

    added tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,23 @@
 using ApproxMasEq
 using Test
 
+N = 10
+alpha = 2
+K = 3
+c = K * alpha
+p0 = 0.5
+seed = 1
+
+eta = 1.0
+alg_str = "FMS"
+rf = rate_FMS_KSAT
+rargs = [eta]
+
+answ_CME = CME_KSAT(rf, rargs, build_args_rate_FMS, N=N, K=K, alpha=alpha)
+answ_CDA = CDA_KSAT(rf, rargs, build_args_rate_FMS, N=N, K=K, alpha=alpha)
+
 @testset "ApproxMasEq.jl" begin
-    # Write your tests here.
+    
+    @test answ_CME.u[end][end] > 0 && answ_CME.u[end][end] < 1
+    @test answ_CDA.u[end][end] > 0 && answ_CDA.u[end][end] < 1
 end
