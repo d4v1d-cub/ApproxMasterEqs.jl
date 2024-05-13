3784a6b8e7004fafc319ba28095c83b4d17f96f4
David Machado <davidmachadop@gmail.com>
2024-05-13 15:37:49 -0400

    updated testing

--- src/testing_CDA.jl
+++ src/testing_CDA.jl
@@ -2,18 +2,15 @@ include("./ApproxMasEq.jl")
 using .ApproxMasEq
 using OrdinaryDiffEq, DiffEqCallbacks, Sundials
 
-# println(Threads.nthreads())
 
 N = parse(Int64, ARGS[1])
 alpha = parse(Float64, ARGS[2])
 K = parse(Int64, ARGS[3])
-c = K * alpha
 p0 = parse(Float64, ARGS[4])
 seed = parse(Int64, ARGS[5])
 
 eta = parse(Float64, ARGS[6])
 alg_str = "FMS"
-rf = rate_FMS_KSAT
 rargs = [eta]
 
 t0 = 0.0
@@ -21,20 +18,16 @@ tlim = parse(Float64, ARGS[7])
 abstol = parse(Float64, ARGS[8])
 reltol = parse(Float64, ARGS[9])
 
-# N = 1000
-# alpha = 2.5
-
 saved_eners_CDA = SavedValues(Float64, Float64)
 cb_ener_CDA = SavingCallback(save_ener_CDA, saved_eners_CDA)
 cbs_save_CDA = CallbackSet(cb_ener_CDA)
 
-tspan = [t0, tlim]
 
 method=CVODE_BDF(linear_solver = :GMRES)
 
-answ = CDA_KSAT(rf, rargs, build_args_rate_FMS, N=N, K=K, alpha=alpha, 
-                seed_g=seed, seed_l=seed, tspan=[t0, tlim], cbs_save=cbs_save_CDA, dt_s=1.0, 
-                abstol=abstol, reltol=reltol)
+answ = CDA_KSAT(rate_FMS_KSAT, rargs, build_args_rate_FMS, N=N, K=K, alpha=alpha, 
+                seed_g=seed, seed_l=seed, tspan=[t0, tlim], cbs_save=cbs_save_CDA, dt_s=2.5, 
+                abstol=abstol, reltol=reltol, method=method)
 
 
 fileener = "../Test/CDA_KSAT_" * alg_str * "_K_" * string(K) * "_N_" * string(N) * "_alpha_" * 
--- src/testing_CME.jl
+++ src/testing_CME.jl
@@ -2,18 +2,15 @@ include("./ApproxMasEq.jl")
 using .ApproxMasEq
 using OrdinaryDiffEq, DiffEqCallbacks, Sundials
 
-# println(Threads.nthreads())
 
 N = parse(Int64, ARGS[1])
 alpha = parse(Float64, ARGS[2])
 K = parse(Int64, ARGS[3])
-c = K * alpha
 p0 = parse(Float64, ARGS[4])
 seed = parse(Int64, ARGS[5])
 
 eta = parse(Float64, ARGS[6])
 alg_str = "FMS"
-rf = rate_FMS_KSAT
 rargs = [eta]
 
 t0 = 0.0
@@ -21,19 +18,16 @@ tlim = parse(Float64, ARGS[7])
 abstol = parse(Float64, ARGS[8])
 reltol = parse(Float64, ARGS[9])
 
-# N = 1000
-# alpha = 2.5
 
 
 saved_eners_CME = SavedValues(Float64, Float64)
 cb_ener_CME = SavingCallback(save_ener_CME, saved_eners_CME)
 cbs_save_CME = CallbackSet(cb_ener_CME)
 
-tspan = [t0, tlim]
 
 method=CVODE_BDF(linear_solver = :GMRES)
 
-answ = CME_KSAT(rf, rargs, build_args_rate_FMS, N=N, K=K, alpha=alpha, 
+answ = CME_KSAT(rate_FMS_KSAT, rargs, build_args_rate_FMS, N=N, K=K, alpha=alpha, 
                 seed_g=seed, seed_l=seed, tspan=[t0, tlim], cbs_save=cbs_save_CME, dt_s=2.5, 
                 abstol=abstol, reltol=reltol, method=method)
 
