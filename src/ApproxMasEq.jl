bd0c96c29fa36828acec174dcc98c1a10f69343c
David Machado <davidmachadop@gmail.com>
2024-05-13 15:16:04 -0400

    exported reshape functions

--- src/ApproxMasEq.jl
+++ src/ApproxMasEq.jl
@@ -14,6 +14,7 @@ include("./CDA.jl")
 
 export CME_KSAT, CDA_KSAT, HGraph, build_ER_HGraph, build_RR_HGraph, gen_links, print_ener, 
        ener, rate_FMS_KSAT, build_args_rate_FMS, build_empty_graph, save_ener_CME, save_ener_CDA, 
-       get_pju_CDA, get_pju_CME, get_graph, get_ch_u, get_ch_u_cond
+       get_pju_CDA, get_pju_CME, get_graph, get_ch_u, get_ch_u_cond, reshape_u_to_probs_CME, 
+       reshape_u_to_probs_CDA
 
 end
