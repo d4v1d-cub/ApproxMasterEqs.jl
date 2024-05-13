c3abe2a479ce1c20c4d1752eb775a1c533bf71e9
David Machado <davidmachadop@gmail.com>
2024-05-13 14:12:08 -0400

    updated test

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,15 +4,13 @@ using Test
 N = 10
 alpha = 2
 K = 3
-c = K * alpha
-p0 = 0.5
 seed = 1
 
 eta = 1.0
 rargs = [eta]
 
-answ_CME = CME_KSAT(rate_FMS_KSAT, rargs, build_args_rate_FMS, N=N, K=K, alpha=alpha)
-answ_CDA = CDA_KSAT(rate_FMS_KSAT, rargs, build_args_rate_FMS, N=N, K=K, alpha=alpha)
+answ_CME = CME_KSAT(rate_FMS_KSAT, rargs, build_args_rate_FMS, N=N, K=K, alpha=alpha, seed_g=seed)
+answ_CDA = CDA_KSAT(rate_FMS_KSAT, rargs, build_args_rate_FMS, N=N, K=K, alpha=alpha, seed_g=seed)
 
 @testset "ApproxMasEq.jl" begin
     
